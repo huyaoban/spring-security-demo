2280ab9d32edcb73ce1c12254f0c97a8155e7e6e
jerry.hu <jerry.hu@sunvalley.com.cn>
2020-03-17 18:16:11 +0800

    配置自定义表单登录页

---
+++ src/main/java/com/huyaoban/security/configuration/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.huyaoban.security.configuration;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// 认证所有请求,认证通过的用户可访问
+		// 设置登录页,登陆页不设访问权限
+		// 关闭跨域
+		http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/myLogin.html").permitAll()
+				.and().csrf().disable();
+	}
+
+}
