f7db154c1badc6a21491a71153574d7ba66cd132
huyaoban <huyaoban@163.com>
2020-03-17 22:04:39 +0800

    资源准备及授权配置

--- src/main/java/com/huyaoban/security/configuration/WebSecurityConfig.java
+++ src/main/java/com/huyaoban/security/configuration/WebSecurityConfig.java
@@ -1,57 +1,19 @@
 package com.huyaoban.security.configuration;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// 认证所有请求,认证通过的用户可访问
-		// 设置登录页,登陆页不设访问权限
-		// 关闭跨域
-		http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/myLogin.html")
-				// 指定处理登录请求的路径，form表单中的action，指定登录成功时的处理逻辑
-				.loginProcessingUrl("/login").successHandler(new AuthenticationSuccessHandler() {
-					@Override
-					public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-							Authentication authentication) throws IOException, ServletException {
-						response.setContentType("application/json;charset=utf-8");
-
-						// 登录成功，输出登录结果
-						PrintWriter out = response.getWriter();
-						out.write("{\"error_code\":\"0\", \"message\":\"欢迎登录系统\"}");
-					}
-
-					// 指定登录失败时的处理逻辑
-				}).failureHandler(new AuthenticationFailureHandler() {
-					@Override
-					public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
-							AuthenticationException exception) throws IOException, ServletException {
-						response.setContentType("application/json;charset=utf-8");
-						response.setStatus(401);
-
-						// 输出失败原因
-						PrintWriter out = response.getWriter();
-						out.write("{\"error_code\":\"401\", \"name\":\"" + exception.getClass() + "\", \"message\":\""
-								+ exception.getMessage() + "\"}");
-					}
-
-				}).permitAll()
-				.and().csrf().disable();
+		// 假设在/admin/api/下的内容是系统后台管理相关的API，在/app/api/下的内容是面向客户端公开访问的API，在/user/api/下的内容是
+		// 用户操作自身数据相关的API；显然，/admin/api必须拥有管理员权限才能进行操作，而/user/api必须在用户登录后才能进行操作。
+		http.authorizeRequests().antMatchers("/admin/api/**").hasRole("ADMIN").antMatchers("/user/api/**")
+				.hasRole("USER").antMatchers("/app/api/**").permitAll().anyRequest().authenticated().and().formLogin()
+				.loginPage("/myLogin.html").loginProcessingUrl("/login").permitAll().and().csrf().disable();
 	}
 
 }
