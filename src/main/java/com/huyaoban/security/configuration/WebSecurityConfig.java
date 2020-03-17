14eadfe19b4cb6cd5c3c0c2ed96c8e960dad91e9
huyaoban <huyaoban@163.com>
2020-03-17 21:16:39 +0800

    指定处理登录请求的路径

--- src/main/java/com/huyaoban/security/configuration/WebSecurityConfig.java
+++ src/main/java/com/huyaoban/security/configuration/WebSecurityConfig.java
@@ -12,7 +12,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		// 认证所有请求,认证通过的用户可访问
 		// 设置登录页,登陆页不设访问权限
 		// 关闭跨域
-		http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/myLogin.html").permitAll()
+		http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/myLogin.html")
+				// 指定处理登录请求的路径，form表单中的action
+				.loginProcessingUrl("/login").permitAll()
 				.and().csrf().disable();
 	}
 
