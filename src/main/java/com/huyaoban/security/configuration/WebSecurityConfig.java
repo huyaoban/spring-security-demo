950727c62d60857256e6e45fb82195016e530ef9
huyaoban <huyaoban@163.com>
2020-03-18 22:40:17 +0800

    使用持久化方案开启自动登录（记住我）

--- src/main/java/com/huyaoban/security/configuration/WebSecurityConfig.java
+++ src/main/java/com/huyaoban/security/configuration/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.huyaoban.security.configuration;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationDetailsSource;
@@ -8,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 
 import com.huyaoban.security.handler.MyAuthenticationFailureHandler;
 import com.huyaoban.security.service.MyUserDetailsService;
@@ -20,8 +22,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private MyUserDetailsService myUserDetailsService;
 
+	@Autowired
+	private DataSource dataSource;
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+		jdbcTokenRepository.setDataSource(dataSource);
+		jdbcTokenRepository.setCreateTableOnStartup(true);
 
 		http.authorizeRequests().antMatchers("/admin/api/**").hasAnyAuthority("ROLE_ADMIN").antMatchers("/user/api/**")
 				// 开放captcha.jpg的访问权限
@@ -29,7 +37,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.and().formLogin().authenticationDetailsSource(myWebAuthenticationDetailsSource)
 				.loginPage("/myLogin.html").loginProcessingUrl("/auth/form").permitAll()
 				// 启用自动登陆,记住我
-				.failureHandler(new MyAuthenticationFailureHandler()).and().rememberMe().key("blurooo")
+				.failureHandler(new MyAuthenticationFailureHandler()).and().rememberMe()
+				.tokenRepository(jdbcTokenRepository)
 				.userDetailsService(myUserDetailsService).and().csrf().disable();
 
 	}
